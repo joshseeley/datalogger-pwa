0e721bf9ac4150859bfb5df9c5836d23760fec18
Josh Seeley <joshseeley@gmail.com>
2023-12-28 13:57:08 -0400

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,9 @@ const About = () => (
       <Row>
         <Col>       
     <h2>About</h2>
-    <p>This app connects to the PIM test box and initializes a data stream.</p>
-    <p>Disconnect will terminate the data stream. </p>
-    <p>The download button will create a csv of the totalized data set from connection. </p>
+    <p>General info: This app connects to the PIM test box and initializes a data stream. The app contains various graphs and and page displaying all data. </p>
+    <p>Connection: Ensure bluetoth is enabled on phone or pc. After clicking connect, data will be streamind once the "Notifications started..." status is displayed. Disconnect will terminate the data stream. </p>
+    <p>Download data: The download button will create a csv of the totalized data set from time of initial connection. </p>
 
         </Col>
       </Row>
